3f479ba53e4bb780fec140327e6d275212ec062e
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2023-10-04 18:09:04 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from utils import Song
 song_file = './beatles.wav'
 
 song = Song(song_file, duration = 30)
-print(song.getBeatsAndTempo())
+tempo, beats = song.getBeatsAndTempo()
+print("Tempo:",tempo)
+print("Beat timestamps:",beats)
 f0_arr, time_in_sec = song.trackPitchACF()
 print(f0_arr.shape)
